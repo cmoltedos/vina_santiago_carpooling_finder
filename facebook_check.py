b2a7a9afa944b9b444e2160367e13f7c131ec979
cesar <cesar.moltedo@gmail.com>
2019-08-23 16:27:22 -0400

    Include new element (permalink) on cache file

--- facebook_check.py
+++ facebook_check.py
@@ -2,13 +2,15 @@
 import os
 import getpass
 import pickle
+import time
+from pdb import set_trace
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
-import time
+
 if os.name == 'nt':
 	import winsound
 
@@ -61,33 +63,34 @@ def reverse_readline(filename, buf_size=8192):
             yield segment
 
 
-def get_cache(group_id, last_n_post=10):
-	cache_dict = dict()
+def get_cache(group_id, last_n_post=20):
+	cache_set = set()
 	filename = '{folder}cache_fb.csv'.format(folder=DATA_FOLDER)
 	if not os.path.exists(filename):
-		return cache_dict
+		return cache_set
 	for line in reverse_readline(filename):
 		values = line.strip().split(',')
-		if values[2] != group_id:
+		if values[3] != group_id:
 			continue
-		cache_dict[(values[0], values[1])] = (values[3], values[4])
+		cache_set.add(values[1])
 		last_n_post -=1 
 		if last_n_post <= 0:
 			break
-	return cache_dict
+	return cache_set
 
 
 def save_cache(group_id, post_dict):
-	line_format = '{utime},{user},{group},{htime},{comment}\n'
+	line_format = '{utime},{permalink},{user},{group},{htime},{comment}\n'
 	os.makedirs(DATA_FOLDER, exist_ok=True)
 	filename = '{folder}cache_fb.csv'.format(folder=DATA_FOLDER)
-	post_list = sorted(post_dict.items())
+	post_list = sorted(post_dict.items(), key=lambda x: x[1][0])
 	with open(filename, 'ab') as cache_file:
 		for post in post_list:
-			(utime, user), (htime, comment) = post
+			permalink, (utime, user, htime, comment) = post
 			try:
 				cache_file.write(line_format.format(
-					utime=utime, user=user, group=group_id, htime=htime, comment=comment).encode('utf8')
+					utime=utime, permalink=permalink, user=user, 
+					group=group_id, htime=htime, comment=comment).encode('utf8')
 				)
 			except UnicodeEncodeError:
 				print("Error in line: {line}".format(line=comment))
@@ -152,8 +155,8 @@ def get_last_n_post(driver, group_id, n_post=10):
 	global MEM_CACHE
 	new_cache_dict = dict()
 	if group_id not in MEM_CACHE:
-		cache_dict = get_cache(group_id)
-		MEM_CACHE[group_id] = cache_dict
+		cache_set = get_cache(group_id)
+		MEM_CACHE[group_id] = cache_set
 	scroll_all_way_down()
 	#driver.get_screenshot_as_file('main-page.png')
 	posts = driver.find_elements_by_css_selector('div[role=article]')
@@ -161,8 +164,10 @@ def get_last_n_post(driver, group_id, n_post=10):
 	num = 0
 	for num, post in enumerate(posts):
 		try:
-			author = post.find_elements_by_css_selector('a[data-hovercard*=user]')[1].text
+			people = post.find_elements_by_css_selector('a[data-hovercard*=user]')[1::2] 
+			author = people[0].text
 			content = post.find_elements_by_css_selector('div.userContent')[-1].text
+			link = post.find_elements_by_css_selector('a[href*=permalink]')[0].get_attribute('href')
 			try:
 				content = post.find_element_by_css_selector('div.mtm').text
 			except NoSuchElementException:
@@ -176,18 +181,18 @@ def get_last_n_post(driver, group_id, n_post=10):
 			# driver.get_screenshot_as_file('main-page_%s.png' % num)
 			continue
 		except:
-			import nose.tools;nose.tools.set_trace()
+			set_trace()
 
 		try:
 			utimestamp = post.find_elements_by_css_selector('abbr')[-1].get_attribute('data-utime')
 		except IndexError:
-			import nose.tools;nose.tools.set_trace()
+			set_trace()
 		timestamp = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(float(utimestamp)))
 
-		cache_key = (utimestamp, author)
-		if cache_key not in MEM_CACHE[group_id]:
-			MEM_CACHE[group_id][cache_key] = (timestamp, content)
-			new_cache_dict[cache_key] = (timestamp, content)
+		only_link_id = link.split('/')[-2]
+		if only_link_id not in MEM_CACHE[group_id]:
+			MEM_CACHE[group_id].add(only_link_id)
+			new_cache_dict[only_link_id] = (utimestamp, author, timestamp, content)
 		else:
 			print("[INFO] CACHE HIT")
 			break
